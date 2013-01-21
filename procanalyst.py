074b47377d92fe7c4a433363477304f6ab2b2e1a
Maverick JS <mavjs01@gmail.com>
2013-01-21 18:31:23 +0800

    used separate classes

--- procanalyst.py
+++ procanalyst.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
+import os
+import sys
 import web
 import psutil
-
+from convert import HumanReadable
+from configparse import ParseConfig
 urls = (
         '/', 'Index',
         '/proc', 'ProcList',
@@ -30,47 +33,44 @@ class Index(object):
     """
     Class to render the index a.k.a / path of the web server
     """
+    def __init__(self):
+        self.inet_dev, self.convert, self.username, self.password, self.smtp_srv = ParseConfig().getvals()
+        
     def GET(self):
         render = web.template.render('templates/')
-        device = ['p1p1'] # change this to reflect your network device
-        up = self.bytes2human(psutil.network_io_counters(pernic=True)[device[0]].bytes_sent)
-        down = self.bytes2human(psutil.network_io_counters(pernic=True)[device[0]].bytes_recv)
+        device = self.inet_dev # change this to reflect your network device
         cpus = psutil.NUM_CPUS
-        phymem = self.bytes2human(psutil.TOTAL_PHYMEM)
+        if self.convert:
+            up = HumanReadable(psutil.network_io_counters(pernic=True)[device].bytes_sent).bytes2human()
+            down = HumanReadable(psutil.network_io_counters(pernic=True)[device].bytes_recv).bytes2human()
+            phymem = HumanReadable(psutil.TOTAL_PHYMEM).bytes2human()
+        else:
+            up = psutil.network_io_counters(pernic=True)[device].bytes_sent
+            down = psutil.network_io_counters(pernic=True)[device].bytes_recv
+            phymem = psutil.TOTAL_PHYMEM
+
         disks_name = self.get_disk_usage().keys()
         disks = self.get_disk_usage()
         return render.index(up, down, cpus, phymem, disks, disks_name)
     
-    def bytes2human(self,n):
-        """
-        converts insane bytes into proper _human_ readable units
-        - taken from https://psutil.googlecode.com/svn/trunk/examples/disk_usage.py
-        - http://code.activestate.com/recipes/578019
-        >>> bytes2human(10000)
-        '9.8K'
-        >>> bytes2human(100001221)
-        '95.4M'
-        """
-        symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
-        prefix = {}
-        for i, s in enumerate(symbols):
-            prefix[s] = 1 << (i+1)*10
-        for s in reversed(symbols):
-            if n >= prefix[s]:
-                value = float(n) / prefix[s]
-                return '%.1f%s' % (value, s)
-        return "%sB" % n
 
     def get_disk_usage(self):
         disk_usages = {}
         for i in psutil.disk_partitions():
             usage = psutil.disk_usage(i.mountpoint)
-            usage_total = self.bytes2human(usage.total)
-            usage_used = self.bytes2human(usage.used)
+            if self.convert:
+                usage_total = HumanReadable(usage.total).bytes2human()
+                usage_used = HumanReadable(usage.used).bytes2human()
+            else:
+                usage_total = usage.total
+                usage_used = usage.used
             disk_usages[i.mountpoint] = {'total':usage_total,'used':usage_used,'fstype':i.fstype}
         return disk_usages
 
 
-if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.run()
+if not os.getuid() == 0:
+    sys.exit('\n[WARNING!] This needs to run as root/administrator! [WARNING!]\n')
+else:
+    if __name__ == "__main__":
+        app = web.application(urls, globals())
+        app.run()
