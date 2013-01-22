ebcbfc9a8e709de9fb880a2061a79a7ea8ec757f
Maverick JS <mavjs01@gmail.com>
2013-01-22 15:15:25 +0800

    updated configparse.py

--- configparse.py
+++ configparse.py
@@ -8,12 +8,23 @@ class ParseConfig():
         self.username = ''
         self.password = ''
         self.smtp_srv = ''
-
-    def getvals(self):
+        self.port = ''
+        self.to = ''
+        self.subject = ''
+        
+        #read config file
         self.parser.read('config.ini')
+
+    def getmiscvar(self):
         self.inet_dev = self.parser.get('misc', 'inet_dev')
         self.convert = self.parser.get('misc', 'convert')
+        return self.inet_dev, self.convert
+    
+    def getmailvar(self):
         self.username = self.parser.get('mail_options', 'username')
         self.password = self.parser.get('mail_options', 'password')
         self.smtp_srv = self.parser.get('mail_options', 'smtp_srv')
-        return self.inet_dev, self.convert, self.username, self.password, self.smtp_srv
+        self.port = self.parser.get('mail_options', 'port')
+        self.to = self.parser.get('mail_options', 'to')
+        self.subject = self.parser.get('mail_options', 'subject')
+        return self.username, self.password, self.smtp_srv, self.port, self.to, self.subject
