a73306a6c7caa084554ce08de2a822605eaeffe4
Maverick JS <mavjs01@gmail.com>
2013-01-22 16:18:50 +0800

    windows doesn't like showing pid0

--- csvwrite.py
+++ csvwrite.py
@@ -12,7 +12,10 @@ class ProcCSV():
         f = open(self.csvname,'wb+')
         f.write("ProcID, ProcName,ProcUser, ProcExe, ProcStatus")
         for proc in proclist:
-            f.write("%s,%s,%s,%s,%s" % (proc.id, proc.name, proc.username, proc.exe, proc.status))
+            if proc.pid == 0:
+                pass
+            else:
+                f.write("%s,%s,%s,%s,%s" % (proc.id, proc.name, proc.username, proc.exe, proc.status))
         f.close()
 
     def sendcsv(self):
