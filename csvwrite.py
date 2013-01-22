371b5193f027bb107124706966ea308bb3c5760e
Maverick JS <mavjs01@gmail.com>
2013-01-22 15:27:26 +0800

    write to csv and send module

---
+++ csvwrite.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import psutil
+from emailsend import AdminSend
+
+class ProcCSV():
+    def __init__(self):
+        self.csvname = 'procanalyst.csv'
+
+    def writecsv(self):
+        proclist = psutil.get_process_list()
+        f = open(self.csvname,'wb+')
+        f.write("ProcID, ProcName,ProcUser, ProcExe, ProcStatus")
+        for proc in proclist:
+            f.write("%s,%s,%s,%s,%s" % (proc.id, proc.name, proc.username, proc.exe, proc.status))
+        f.close()
+
+    def sendcsv(self):
+        send = AdminSend(self.csvname)
+        send.send()
+
+
+def main():
+    app = ProcCSV()
+    app.writecsv()
+    app.sendcsv()
+
+if __name__ == "__main__":
+    main()
